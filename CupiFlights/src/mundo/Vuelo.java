4cba4a58e59ab4ee16662d2e9d8f22a3680a0d0a
sfrsebastian <sfrsebastian@gmail.com>
2014-04-28 14:11:12 -0500

    Agregadas tardanzas y modificado constructor de vuelo.

--- CupiFlights/src/mundo/Vuelo.java
+++ CupiFlights/src/mundo/Vuelo.java
@@ -1,6 +1,7 @@
 package mundo;
 
 import java.io.Serializable;
+import java.util.Calendar;
 
 public class Vuelo implements Serializable,Comparable<Vuelo> {
 	public final static String SALIENDO = "Saliendo";
@@ -10,28 +11,69 @@ public class Vuelo implements Serializable,Comparable<Vuelo> {
 	 */
 	private static final long serialVersionUID = 7370494462035777075L;
 	private String numero;
-	private String codigoSalida;
-	private String codigoLlegada;
 	private String tipo;
-	private String codigoAereolinea;
+	private double rating;
+	private Aeropuerto salida;
+	private Aeropuerto llegada;
+	private Aerolinea aerolinea;
+	private int l15;
+	private int l30;
+	private int l45;
+	private int cancelados;
+	private Calendar fecha;
 	
-	public Vuelo(String nNumero, String nCodigoSalida, String nCodigoLlegada, String nTipo, String nCodigoAereolinea) {
+	public Vuelo(String nNumero, Aeropuerto nSalida, Aeropuerto nLlegada, Aerolinea nAereolinea, String nTipo, Object[] nRating, Calendar c) {
+		fecha = c;
 		numero = nNumero;
-		codigoSalida = nCodigoSalida;
-		codigoLlegada = nCodigoLlegada;
+		salida = nSalida;
+		llegada = nLlegada;
 		tipo = nTipo;
-		codigoAereolinea = nCodigoAereolinea;
+		aerolinea = nAereolinea;
+		rating = (double) nRating[0];
+		l15 = (int) nRating[1];
+		l30 = (int) nRating[2];
+		l45 = (int) nRating[3];
+		cancelados = (int) nRating[4];
+		
+	}
+	
+	public Vuelo(String nNumero){
+		numero = nNumero;
+	}
+	public Aeropuerto getSalida() {
+		return salida;
+	}
+	public Aeropuerto getLlegada() {
+		return llegada;
+	}
+	public Aerolinea getAereolinea() {
+		return aerolinea;
 	}
 	public String getNumero() {
 		return numero;
 	}
-	public String getCodigoSalida() {
-		return codigoSalida;
+	public String getTipo() {
+		return tipo;
 	}
-	public String getCodigoLlegada() {
-		return codigoLlegada;
+	public double getRating(){
+		return rating;
 	}
-	@Override
+	public int getL15() {
+		return l15;
+	}
+	public int getL30() {
+		return l30;
+	}
+	public int getL45() {
+		return l45;
+	}
+	public int getCancelados() {
+		return cancelados;
+	}
+	
+	public String toString(){
+		return numero + " " + aerolinea.toString();
+	}
 	public int compareTo(Vuelo o) {
 		int comparacion = numero.compareTo(o.numero);
 		if(comparacion>0){
@@ -44,12 +86,5 @@ public class Vuelo implements Serializable,Comparable<Vuelo> {
 			return 0;
 		}
 	}
-	public String getTipo() {
-		return tipo;
-	}
-	public String getAerolinea() {
-		return codigoAereolinea;
-	}
-
 	
 }
