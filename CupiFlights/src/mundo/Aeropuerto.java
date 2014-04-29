0984722a54c82a0e90b5a51e357babe8087bba41
sfrsebastian <sfrsebastian@gmail.com>
2014-04-28 23:23:52 -0500

    Clase aeropuerto implementada y documentada

--- CupiFlights/src/mundo/Aeropuerto.java
+++ CupiFlights/src/mundo/Aeropuerto.java
@@ -3,27 +3,96 @@ package mundo;
 import java.io.Serializable;
 import java.util.Iterator;
 
-import Arbol23.Arbol23;
+import ArbolTrie.ArbolTrie;
 
 public class Aeropuerto implements Comparable<Aeropuerto>,Serializable{
-
-	public static final String NOTIENE = "No tiene";
-	private String codigoCiudad;
-	private String codigoPais;
-	private String nombre;
-	private String ciudad;
-	private String pais;
-	private double tardanza;
-	private String latitud;
-	private String longitud;
+	//--------------------
+	//CONSTANTES
+	//--------------------
 	/**
-	 * Codigo flightstats
+	 * Serial identificador de la clase
+	 */
+	private static final long serialVersionUID = -1054162308338049538L;
+
+	/**
+	 * Constante que indica que no se tiene codigo de ciudad
+	 */
+	public static final String NOTIENE = "No tiene";
+
+	//--------------------
+	//ATRIBUTOS
+	//--------------------
+	/**
+	 * El codigo de la ciudad del aeropuerto
+	 */
+	private String codigoCiudad;
+
+	/**
+	 * Codigo del pais del aeropuerto
+	 */
+	private String codigoPais;
+
+	/**
+	 * Nombre del aeropuerto
+	 */
+	private String nombre;
+
+	/**
+	 * Nombre de la ciudad del aeropuerto
+	 */
+	private String ciudad;
+
+	/**
+	 * Nombre del pais del aeropuerto
+	 */
+	private String pais;
+
+	/**
+	 * Indice de tardanza del aeropuerto
+	 */
+	private double tardanza;
+
+	/**
+	 * Latitud del aeropuerto
+	 */
+	private String latitud;
+
+	/**
+	 * Longitud del aeropuerto
+	 */
+	private String longitud;
+
+	/**
+	 * Codigo flightstats identificador
 	 */
 	private String codigo;
-	
-	private Arbol23<Vuelo> vuelosEntrada;
-	
-	private Arbol23<Vuelo> vuelosSalida;
+
+	/**
+	 * Vuelos de entrada del aeropuerto, organizados por fecha
+	 */
+	private ArbolTrie<Vuelo> vuelosEntrada;
+
+	/**
+	 * vuelos de salida del aeropuerto, organizados por fecha
+	 */
+	private ArbolTrie<Vuelo> vuelosSalida;
+
+	//--------------------
+	//CONSTRUCTORES
+	//--------------------
+	/**
+	 * Crea un nuevo aeropuerto con los parametros dados.<br>
+	 * Se inicializan vacias las contenedoras de vuelos.
+	 * @param nNombre El nombre del aeropuerto
+	 * @param nCiudad La ciudad del aeropuerto
+	 * @param nPais El pais del aeropuerto
+	 * @param nCodigoCiudad El codigo de ciudad del aeropuerto
+	 * @param nCodigoPais El codigo de pais del aeropuerto
+	 * @param nCodigo El codigo identificador del aeropuerto
+	 * @param nTardanza El indice de tardanza del aeropuerto
+	 * @param nLatitud La latitud del aeropuerto
+	 * @param nLongitud La longitud del aeropuerto
+	 */
 	public Aeropuerto(String nNombre, String nCiudad,String nPais, String nCodigoCiudad,String nCodigoPais,String nCodigo, double nTardanza,String nLatitud, String nLongitud){
 		nombre = nNombre;
 		ciudad = nCiudad;
@@ -32,48 +101,97 @@ public class Aeropuerto implements Comparable<Aeropuerto>,Serializable{
 		codigoPais = nCodigoPais;
 		codigo = nCodigo;
 		tardanza = nTardanza;
-		vuelosEntrada = new Arbol23<Vuelo>();
-		vuelosSalida = new Arbol23<Vuelo>();
+		vuelosEntrada = new ArbolTrie<Vuelo>();
+		vuelosSalida = new ArbolTrie<Vuelo>();
 		latitud = nLatitud;
 		longitud = nLongitud;
 	}
-	
+
+	/**
+	 * Crea un aeropuerto a partir del codigo dado de parametro
+	 * @param nCodigo
+	 */
 	public Aeropuerto(String nCodigo){
 		codigo = nCodigo;
 	}
-	
-	public boolean agregarVueloEntrada(Vuelo vuelo) throws Exception{
-		return vuelosEntrada.agregar(vuelo);
+
+	//--------------------
+	//METODOS
+	//--------------------
+	/**
+	 * Agrega el vuelo dado por parametro con la fecha indicada al arbol de entradas.
+	 * @param fecha La fecha del vuelo
+	 * @param vuelo El vuelo a agregar
+	 * @return TRUE si agrego, FALSE de lo contrario
+	 * @throws Exception
+	 */
+	public boolean agregarVueloEntrada(String fecha ,Vuelo vuelo) throws Exception{
+		return vuelosEntrada.agregar(fecha, vuelo);
 	}
-	
-	public boolean agregarVueloSalida(Vuelo vuelo) throws Exception{
-		return vuelosSalida.agregar(vuelo);
+
+	/**
+	 * Agrega el vuelo dado por parametro con la fecha indicada al arbol de salidas.
+	 * @param fecha La fecha del vuelo
+	 * @param vuelo El vuelo a agregar
+	 * @return TRUE si agrego, FALSE de lo contrario
+	 * @throws Exception
+	 */
+	public boolean agregarVueloSalida(String fecha, Vuelo vuelo) throws Exception{
+		return vuelosSalida.agregar(fecha, vuelo);
 	}
+
+	/**
+	 * Retorna un iterador con los vuelos que entran al aeropuerto
+	 * @return Iterador de vuelos
+	 */
 	public Iterator<Vuelo> darVuelosEntrada(){
-		return vuelosEntrada.iterator();
+		return vuelosEntrada.buscarXPrefijo("");
 	}
-	
+
+	/**
+	 * Retorna un iterador con los vuelos que salen del aeropuerto
+	 * @return Iterador de vuelos
+	 */
 	public Iterator<Vuelo> darVuelosSalida(){
-		return vuelosSalida.iterator();
+		return vuelosSalida.buscarXPrefijo("");
 	}
-	
-	public Arbol23<Vuelo> getVuelosEntrada(){
+
+	/**
+	 * Retorna los vuelos de entrada del aeropuerto
+	 * @return Arbol trie con los vuelos
+	 */
+	public ArbolTrie<Vuelo> getVuelosEntrada(){
 		return vuelosEntrada;
 	}
-	
-	public Arbol23<Vuelo> getVuelosSalida(){
+
+	/**
+	 * Retorna los vuelos de salida del arbol
+	 * @return Arbol trie con los vuelos
+	 */
+	public ArbolTrie<Vuelo> getVuelosSalida(){
 		return vuelosSalida;
 	}
+
+	/**
+	 * Retorna el codigo de ciudad del aeropuerto
+	 * @return El codigo del aeropuerto
+	 */
 	public String getCodigoCiudad() {
 		return codigoCiudad;
 	}
 
-
+	/**
+	 * Retorna el codigo de pais del aeropuerto
+	 * @return El codigo del pais
+	 */
 	public String getCodigoPais() {
 		return codigoPais;
 	}
 
-
+	/**
+	 * Retorna el nombre del aeropuerto
+	 * @return El nombre del aeropuerto
+	 */
 	public String getNombre() {
 		return nombre;
 	}
@@ -83,32 +201,57 @@ public class Aeropuerto implements Comparable<Aeropuerto>,Serializable{
 		return ciudad;
 	}
 
-
+	/**
+	 * Retorna el nombre del pais del aeropuerto
+	 * @return El pais del aeropuerto
+	 */
 	public String getPais() {
 		return pais;
 	}
 
-
+	/**
+	 * Retorna el indice de tardanza del aeropuerto
+	 * @return El indice de tardanza
+	 */
 	public double getTardanza() {
 		return tardanza;
 	}
 
-
+	/**
+	 * Retorna el codigo del aeropuerto
+	 * @return El codigo del aeropuerto
+	 */
 	public String getCodigo() {
 		return codigo;
 	}
 
+	/**
+	 * Retorna la latitud del aeropuerto
+	 * @return La latitud del aeropuerto
+	 */
 	public String getLatitud(){
 		return latitud;
 	}
+
+	/**
+	 * Retorna la longitud del aeropeurto
+	 * @return La longitud del aeropuerto
+	 */
 	public String getLongitud(){
 		return longitud;
 	}
-	
+
+	/**
+	 * Metodo toString del aeropuerto<br>
+	 * Retorna el nombre del aeropuerto
+	 */
 	public String toString(){
 		return nombre;
 	}
-	@Override
+
+	/**
+	 * Metodo de comparacion de dos aeropuertos a partir de su codigo
+	 */
 	public int compareTo(Aeropuerto o) {
 		int comparacion = codigo.compareTo(o.codigo);
 		if(comparacion>0){
@@ -121,5 +264,4 @@ public class Aeropuerto implements Comparable<Aeropuerto>,Serializable{
 			return 0;
 		}
 	}
-
 }
