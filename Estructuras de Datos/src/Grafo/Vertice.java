663c8c98ad3d156a76aaa970fff08c970921bdeb
sfrsebastian <sfrsebastian@gmail.com>
2014-05-11 15:59:37 -0500

    Getters info, marca y predecesores implementados y documentados

--- Estructuras de Datos/src/Grafo/Vertice.java
+++ Estructuras de Datos/src/Grafo/Vertice.java
@@ -1,5 +1,10 @@
 package Grafo;
 
+import java.util.Iterator;
+
+import Lista.Lista;
+import ListaEncadenada.ListaEncadenada;
+
 public class Vertice<K, V, A> implements Comparable<Vertice<K, V, A>>{
 
 	//---------------------------------------
@@ -12,15 +17,29 @@ public class Vertice<K, V, A> implements Comparable<Vertice<K, V, A>>{
 	private K idVertice;
 	
 	/**
-	 * La informacion que tiene el vertice
+	 * El elemento del vertice
 	 */
-	private V infoVertice;
+	private V elemento;
+	
+	/**
+	 * La informacion del vertice
+	 */
+	private A infoVertice;
 	
 	/**
 	 * Indica si el nodo se encuentra marcado
 	 */
 	private boolean marcado;
 	
+	/**
+	 * Los arcos sucesores del vertice
+	 */
+	private Lista<Arco<K,V,A>> sucesores;
+	
+	/**
+	 * Los arcos sucesores del vertice
+	 */
+	private Lista<Arco<K,V,A>> predecesores;
 	//---------------------------------------
 	// Constructor
 	//---------------------------------------
@@ -30,15 +49,41 @@ public class Vertice<K, V, A> implements Comparable<Vertice<K, V, A>>{
 	 * @param id El identificador del vertice
 	 * @param info La informacion que tiene el vertice
 	 */
-	public Vertice(K id, V info){
+	public Vertice(K id, V nElemento, A info){
 		idVertice = id;
+		elemento = nElemento;
 		infoVertice = info;
+		sucesores = new ListaEncadenada<Arco<K,V,A>>();
+		predecesores = new ListaEncadenada<Arco<K,V,A>>();
 		marcado = false;
 	}
 	
 	//---------------------------------------
 	// Metodos
 	//---------------------------------------
+	/**
+	 * Retorna la informacion del vertice.
+	 * @return La informacion del vertice
+	 */
+	public A getInfo(){
+		return infoVertice;
+	}
+	
+	/**
+	 * Indica si el vertice esta marcado o no.
+	 * @return TRUE si esta marcado, FALSE de lo contrario.
+	 */
+	public boolean darMarca(){
+		return marcado;
+	}
+	
+	/**
+	 * Retorna un iterador de los sucedores del vertice.
+	 * @return Los sucesores del vertice.
+	 */
+	public Iterator<Arco<K,V,A>> darSucesores(){
+		return sucesores.iterator();
+	}
 	
 	/**
 	 * Marca el nodo
