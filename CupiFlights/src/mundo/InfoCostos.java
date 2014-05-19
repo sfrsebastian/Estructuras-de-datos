4b189a010ce922111f432ec11cc6720251df0161
sfrsebastian <sfrsebastian@gmail.com>
2014-05-19 10:11:14 -0500

    Clase de información de costos agregada e implementada

---
+++ CupiFlights/src/mundo/InfoCostos.java
@@ -0,0 +1,54 @@
+package mundo;
+
+import Grafo.IInfoArco;
+
+public class InfoCostos implements IInfoArco {
+	
+	private static final String DISTANCIA = "Distancia";
+	private static final String TIEMPO = "Tiempo";
+	private static final String RATING = "Rating";
+	private static final String TARDIOS = "Vuelos Tardios";
+	
+	private float distancia;
+	private float tiempo;
+	private float rating;
+	private float tardios;
+	private Vuelo vuelo;
+	
+	public InfoCostos(float nDistancia, float nTiempo, float nRating, float nTardios, Vuelo nVuelo){
+		distancia = nDistancia;
+		tiempo = nTiempo;
+		rating = nRating;
+		tardios = nTardios;
+	}
+	
+	@Override
+	public float getCosto() {
+		return getCosto(" ");
+	}
+
+	@Override
+	public float getCosto(String arg) {
+		if(arg.equals(RATING)){
+			return rating;
+		}
+		else if(arg.equals(TARDIOS)){
+			return tardios;
+		}
+		else if(arg.equals(TIEMPO)){
+			return tiempo;
+		}
+		else{
+			return distancia;
+		}
+	}
+
+	public Vuelo getVuelo(){
+		return vuelo;
+	}
+	
+	@Override
+	public int compareTo(IInfoArco o) {
+		return ((InfoCostos) o).getVuelo().compareTo(vuelo);
+	}
+}
